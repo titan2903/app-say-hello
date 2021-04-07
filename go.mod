85c811a99853ad96df9b911006fd444a09acbbf7
Titanio Yudista <52256179+titan2903@users.noreply.github.com>
2021-04-07 09:22:17 +0700

    app simple go with go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/titan2903/app-say-hello
+
+go 1.16
+
+require github.com/titan2903/go-say-hallo/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/titan2903/go-say-hallo/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello(" titan "))
+}
